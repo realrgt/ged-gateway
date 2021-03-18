6da137175dd208695d0109314a7308e353eeee35
realrgt <ergitorubildo@gmail.com>
2021-03-18 09:30:19 +0200

    authentication completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MpesaPaymentModule } from './mpesa-payment/mpesa-payment.module';
+import { HeaderComponent } from './core/header/header.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MpesaPaymentModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+
+import firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { User } from '../models/user.model';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  user$: Observable<User | null | undefined>;
+
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap((user) => {
+        if (user) {
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
+
+  async googleSignIn(): Promise<void> {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  async facebookSignIn(): Promise<void> {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    const credential = await this.afAuth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  async signOut(): Promise<void> {
+    await this.afAuth.signOut();
+  }
+
+  private updateUserData({
+    uid,
+    email,
+    displayName,
+    photoURL,
+  }: any): Promise<void> {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(
+      `users/${uid}`
+    );
+
+    const data = {
+      uid,
+      email,
+      displayName,
+      photoURL,
+    };
+
+    return userRef.set(data, { merge: true });
+  }
+}
