c142fa9e2053074b92c6224e1b9641d2bc8f07a9
realrgt <ergitorubildo@gmail.com>
2021-03-20 11:23:30 +0200

    header outfit

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 // imports
 const express = require("express");
 const cors = require("cors");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 // initializations
 const app = express();
@@ -10,6 +10,7 @@ dotenv.config();
 // routes imports here
 const appRoutes = require("./routes/app");
 const transactionRoutes = require("./routes/transaction");
+const converterRoutes = require("./routes/converter");
 
 // allow application to be consumed
 app.use(cors());
@@ -20,11 +21,12 @@ app.use(express.json());
 
 // routes
 app.use("/transaction", transactionRoutes);
+app.use("/converter", converterRoutes);
 app.use("/", appRoutes);
 
 // executes queries
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("Server in port "+ port +": \x1b[32m%s\x1b[0m", "online");
+  console.log("Server in port " + port + ": \x1b[32m%s\x1b[0m", "online");
 });
---
+++ server/src/routes/converter.js
@@ -0,0 +1,33 @@
+const { response } = require("express");
+const express = require("express");
+var request = require("request");
+var fs = require("fs");
+var mm = require("musicmetadata");
+
+const router = express.Router();
+
+router.post("/", (req, res, next) => {
+  const musics = req.body.musics;
+
+  request(
+    {
+      url: musics[0],
+    },
+    (err, response, body) => {
+      console.log(response.statusCode);
+      var parser = new mm(fs.createReadStream("sample.mp3"));
+      parser.on("metadata", function (result) {
+        console.log(result);
+      });
+      process.exit(0);
+    }
+  );
+
+  res.status(200).json({
+    ok: true,
+    message: "Request executed successfully",
+    musics,
+  });
+});
+
+module.exports = router;
