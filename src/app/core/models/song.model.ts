88dc1f84ac82b7d20e29dabf2e33441350eb3971
realrgt <ergitorubildo@gmail.com>
2021-03-21 01:21:28 +0200

    metadata style complete

---
+++ src/app/core/models/song.model.ts
@@ -0,0 +1,7 @@
+export interface Song {
+  artist: string;
+  year: number;
+  archives: string[];
+  album?: string;
+  albumArt?: string;
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Song } from '../core/models/song.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   displayPurchase: boolean = false;
-  musicList: string[] = [
-    'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
-    'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
+
+  songs: Song[] = [
+    {
+      artist: 'Hillsong United',
+      album: 'People',
+      year: 2016,
+      albumArt:
+        'https://i1.wp.com/trevordecker.com/wp-content/uploads/2019/04/Hillsong-United-People-2019-English-Christian-Live-Album.jpg?w=1200&ssl=1',
+      archives: [
+        'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
+        'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
+      ],
+    },
+    {
+      artist: 'Justin Bieber',
+      album: 'Justice',
+      year: 2021,
+      albumArt:
+        'https://twitter.com/justinbieber/status/1372760718500458496/photo/1',
+      archives: [
+        'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
+        'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
+      ],
+    },
   ];
 
   constructor() {}
