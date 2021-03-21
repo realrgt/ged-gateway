47052fb51996b66e0b200ddc7a891c0659ac63df
realrgt <ergitorubildo@gmail.com>
2021-03-21 17:36:34 +0200

    playlist

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,30 +9,18 @@ import { Song } from '../core/models/song.model';
 export class HomeComponent implements OnInit {
   displayPurchase: boolean = false;
 
-  songs: Song[] = [
-    {
-      artist: 'Hillsong United',
-      album: 'People',
-      year: 2016,
-      albumArt:
-        'https://i1.wp.com/trevordecker.com/wp-content/uploads/2019/04/Hillsong-United-People-2019-English-Christian-Live-Album.jpg?w=1200&ssl=1',
-      archives: [
-        'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
-        'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
-      ],
-    },
-    {
-      artist: 'Justin Bieber',
-      album: 'Justice',
-      year: 2021,
-      albumArt:
-        'https://twitter.com/justinbieber/status/1372760718500458496/photo/1',
-      archives: [
-        'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
-        'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
-      ],
-    },
-  ];
+  song: Song = {
+    artist: 'Hillsong United',
+    album: 'People',
+    year: 2016,
+    albumArt:
+      'https://i1.wp.com/trevordecker.com/wp-content/uploads/2019/04/Hillsong-United-People-2019-English-Christian-Live-Album.jpg?w=1200&ssl=1',
+    archives: [
+      'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
+      'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
+      'http://gedlab.handza.co.mz/wp-content/uploads/2021/03/Raimundo-Sive-Yehova-Una-Hina2.mp3',
+    ],
+  };
 
   constructor() {}
 
